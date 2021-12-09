5bed95804f9530d9995aba19687b4a9ccdfe4dda
Caldrakke <86433023+Caldrakke@users.noreply.github.com>
2021-12-09 12:58:40 -0500

    Add files via upload

---
+++ src/main/java/cl/twk/primerdesarrollo/PrimerdesarrolloApplication.java
@@ -0,0 +1,13 @@
+package cl.twk.primerdesarrollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerdesarrolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerdesarrolloApplication.class, args);
+	}
+
+}
